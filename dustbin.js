95468fc0e0fe235c681dcebd399987cb8748d345
ritwiksaraf28062008 <62437795+ritwiksaraf28062008@users.noreply.github.com>
2020-08-31 13:09:14 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,38 @@
+class Dustbin {
+    constructor(x,y,w,h) {
+        var options = {
+            friction:0.5,
+            restitution: 0.3,
+            isStatic:false,
+            density:0.2 
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        World.add(world, this.body);
+        this.width = w;
+        this.height = h;   
+    }
+
+    
+    display(){
+    var pos =this.body.position;
+    if(this.body.speed < 3){
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x, pos.y,this.width,this.height);
+       }
+       else{
+         World.remove(world, this.body);
+         push();
+         this.Visiblity = this.Visiblity - 5;
+         pop();
+       }
+      }
+
+      score(){
+        if(this.body.Visiblity <= 0){
+          score = score+1
+        }   
+      }
+
+     
+}
